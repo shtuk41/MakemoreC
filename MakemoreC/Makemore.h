9a802ee73b2e74bd6ed27bc8427eed6cc7ba8309
shtuk41 <shtuk41@gmail.com>
2023-08-24 20:51:43 -0500

    created Makemore class to include logic bigrams structures

---
+++ MakemoreC/Makemore.h
@@ -0,0 +1,120 @@
+#pragma once
+
+#include <list>
+#include <ranges>
+#include <set>
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+//bool comp(std::pair<std::pair<char, char>, int> a, std::pair<std::pair<char, char>, int> b) {
+//	return a.second < b.second;
+//}
+
+class BigramHashFunction
+{
+public:
+	size_t operator()(const std::pair<char, char>& p) const
+	{
+		return (size_t)(int(p.first) * int(p.second));
+	}
+};
+
+class Makemore
+{
+private:
+	std::vector<std::string> originalNames;
+	std::vector<std::string> names;
+	std::list<char> characters;
+	std::map<char, int> stoi;
+	std::unordered_map<std::pair<char, char>, int, BigramHashFunction> bigramMap;
+	std::vector<std::pair<std::pair<char, char>, int>> bigramVector;
+
+
+public:
+	void Init(std::string fileNames)
+	{
+		std::ifstream namesFile(fileNames);
+
+		if (namesFile.is_open())
+		{
+			std::string name;
+			std::string single;
+			while (std::getline(namesFile, name))
+			{
+				originalNames.push_back(name);
+				names.push_back(std::string("@") + name + "$");
+				single += name;
+			}
+
+			std::set<char> singleSet;
+
+			for (auto ii : single)
+			{
+				singleSet.insert(ii);
+			}
+
+			characters = std::list<char>(singleSet.begin(), singleSet.end());
+			characters.sort();
+
+			int count = 0;
+			for (auto c : characters)
+			{
+				stoi.insert(std::pair<char, int>(c, count));
+				count++;
+			}
+
+			stoi.insert(std::pair<char, int>('@', count++));
+			stoi.insert(std::pair<char, int>('$', count));
+
+			n = new int* [count];
+			for (int i = 0; i < count; i++)
+			{
+				n[i] = new int[count] {0};
+			}
+
+			for (auto ii : names)
+			{
+				for (int jj = 0; jj < ii.length() - 1; jj++)
+				{
+					auto bigram = std::pair<char, char>(ii[jj], ii[jj + 1]);
+
+					auto kk = bigramMap.find(bigram);
+
+					if (kk != bigramMap.end())
+					{
+						kk->second += 1;
+					}
+					else
+					{
+						bigramMap[bigram] = 1;
+					}
+				}
+			}
+
+			bigramVector = std::vector<std::pair<std::pair<char, char>, int>>(bigramMap.begin(), bigramMap.end());
+
+			std::sort(bigramVector.rbegin(), bigramVector.rend(), [](std::pair<std::pair<char, char>, int> a, std::pair<std::pair<char, char>, int> b) {return a.second < b.second; });
+		}
+	}
+
+	~Makemore()
+	{
+		for (int ii = 0; ii < characters.size() + 1; ii++)
+		{
+			delete[] n[ii];
+		}
+
+		delete[] n;
+	}
+
+	std::vector<std::string>& Names() { return names; }
+	std::vector<std::string>& OriginalNames() { return originalNames; }
+	std::list<char>& Characters() { return characters; }
+	std::map<char, int>& Stoi() { return stoi; }
+	std::unordered_map<std::pair<char, char>, int, BigramHashFunction>& BigramMap() { return bigramMap; };
+	std::vector<std::pair<std::pair<char, char>, int>>& BigramVector() { return bigramVector; };
+
+	int** N() { return n; }
+	int** n;
+};
--- MakemoreC/test.cpp
+++ MakemoreC/test.cpp
@@ -1,29 +1,8 @@
 #include "pch.h"
 #include <fstream>
 #include <iostream>
-#include <list>
-#include <ranges>
-#include <set>
-#include <string>
-#include <vector>
-#include <unordered_map>
 
-class Makemore
-{
-private:
-	std::vector<std::string> original;
-	std::vector<std::string> names;
-	std::list<char> characters;
-	std::map<char, int> stoi;
-	
-public:
-	std::vector<std::string> & Names() { return names; }
-	std::vector<std::string>& OriginalNames() { return original; }
-	std::list<char>& Characters() { return characters; }
-	std::map<char, int>& Stoi() { return stoi; }
-	int** N() { return n; }
-	int** n;
- };
+#include <Makemore.h>
 
 class MakemoreTest : public ::testing::Test 
 {
@@ -32,55 +11,11 @@ protected:
 
 	void SetUp() override
 	{
-		std::ifstream namesFile("names.txt");
-
-		if (namesFile.is_open())
-		{
-			std::string name;
-			std::string single;
-			while (std::getline(namesFile, name))
-			{
-				mm.OriginalNames().push_back(name);
-				mm.Names().push_back(std::string("@") + name + "$");
-				single += name;
-			}
-
-			std::set<char> singleSet;
-
-			for (auto ii : single)
-			{
-				singleSet.insert(ii);
-			}
-
-			mm.Characters() = std::list<char>(singleSet.begin(), singleSet.end());
-			mm.Characters().sort();
-
-			int count = 0;
-			for (auto c : mm.Characters())
-			{
-				mm.Stoi().insert(std::pair<char, int>(c, count));
-				count++;
-			}
-
-			mm.Stoi().insert(std::pair<char, int>('@', count++));
-			mm.Stoi().insert(std::pair<char, int>('$', count));
-
-			mm.n = new int* [count];
-			for (int i = 0; i < count; i++)
-			{
-				mm.n[i] = new int[count] {0};
-			}
-		}
+		mm.Init("names.txt");
 	}
 
 	void TearDown() override
 	{
-		for (int ii = 0; ii < mm.Characters().size() + 1; ii++)
-		{
-			delete [] mm.n[ii];
-		}
-
-		delete [] mm.n;
 	}
 };
 
@@ -89,17 +24,6 @@ TEST_F(MakemoreTest, ReadNames)
 	size_t length = mm.Names().size();
 
 	EXPECT_EQ(length, 32033);
-}
-
-TEST_F(MakemoreTest, Print10)
-{
-	auto ten = mm.Names() | std::ranges::views::take(10);
-	
-
-	for (auto i : ten)
-	{
-		std::cout << i << '\n';
-	}
 }
 
 TEST_F(MakemoreTest, FindMinLength)
@@ -120,133 +44,22 @@ TEST_F(MakemoreTest, FindMaxLength)
 	EXPECT_EQ(longest->size(), 15);
 }
 
-TEST_F(MakemoreTest, FindTwoLetterPairs)
-{
-	auto three = mm.Names() | std::ranges::views::take(3);
-	std::vector<std::pair<char, char>> two_letter_pairs;
-
-	for (auto ii : three)
-	{
-		for (int jj = 0; jj < ii.length() - 1; jj++)
-		{
-			two_letter_pairs.push_back(std::pair<char, char>(ii[jj], ii[jj + 1]));
-		}
-	}
-
-	for (auto ii : two_letter_pairs)
-	{
-		std::cout << ii.first << ", " << ii.second << '\n';
-	}
-}
-
-class BigramHashFunction
-{
-public:
-	size_t operator()(const std::pair<char, char>& p) const
-	{
-		return (size_t)(int(p.first) * int(p.second));
-	}
-};
-
-TEST_F(MakemoreTest, BigramStatistics3Words_map)
-{
-	auto three = mm.Names() | std::ranges::views::take(3);
-
-	std::unordered_map<std::pair<char, char>, int, BigramHashFunction> b;
-
-	for (auto ii : three)
-	{
-		for (int jj = 0; jj < ii.length() - 1; jj++)
-		{
-			auto bigram = std::pair<char, char>(ii[jj], ii[jj + 1]);
-
-			auto kk = b.find(bigram);
-
-			if (kk != b.end())
-			{
-				kk->second += 1;
-			}
-			else
-			{
-				b[bigram] = 1;
-			}
-		}
-	}
-
-	for (auto bigram : b)
-	{
-		std::cout << bigram.first.first << "," << bigram.first.second << '=' << bigram.second << '\n';
-	}
-}
-
-TEST_F(MakemoreTest, BigramStatisticsAllWords_map)
-{
-	std::unordered_map<std::pair<char, char>, int, BigramHashFunction> b;
-
-	for (auto ii : mm.Names())
-	{
-		for (int jj = 0; jj < ii.length() - 1; jj++)
-		{
-			auto bigram = std::pair<char, char>(ii[jj], ii[jj + 1]);
-
-			auto kk = b.find(bigram);
-
-			if (kk != b.end())
-			{
-				kk->second += 1;
-			}
-			else
-			{
-				b[bigram] = 1;
-			}
-		}
-	}
-
-	for (auto bigram : b)
-	{
-		std::cout << "new: " << bigram.first.first << "," << bigram.first.second << '=' << bigram.second << '\n';
-	}
-}
-
-bool comp(std::pair<std::pair<char, char>, int> a, std::pair<std::pair<char, char>, int> b) {
-	return a.second < b.second;
-}
-
-
 TEST_F(MakemoreTest, BigramStatisticsAllWords_Sorted)
 {
-	std::unordered_map<std::pair<char, char>, int, BigramHashFunction> b;
 
-	for (auto ii : mm.Names())
-	{
-		for (int jj = 0; jj < ii.length() - 1; jj++)
-		{
-			auto bigram = std::pair<char, char>(ii[jj], ii[jj + 1]);
+	EXPECT_EQ(mm.BigramVector()[0].second, 6763);
+}
 
-			auto kk = b.find(bigram);
+TEST_F(MakemoreTest, DISABLED_PrintAllBigrams_Sorted)
+{
 
-			if (kk != b.end())
-			{
-				kk->second += 1;
-			}
-			else
-			{
-				b[bigram] = 1;
-			}
-		}
-	}
-
-	std::vector<std::pair<std::pair<char, char>, int>> view = std::vector<std::pair<std::pair<char, char>, int>>(b.begin(), b.end());
-
-	std::sort(view.rbegin(), view.rend(), comp);
-
-	for (auto bigram : view)
+	for (auto bigram : mm.BigramVector())
 	{
 		std::cout << bigram.first.first << "," << bigram.first.second << '=' << bigram.second << '\n';
 	}
 }
 
-TEST_F(MakemoreTest, Print_Characters)
+TEST_F(MakemoreTest, DISABLED_Print_Characters)
 {
 	for (auto c : mm.Characters())
 	{
