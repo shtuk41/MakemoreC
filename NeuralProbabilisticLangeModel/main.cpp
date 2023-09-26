1fb18ed222e4e228dff14d2cb958788caa1e04a4
shtuk41 <shtuk41@gmail.com>
2023-09-25 20:43:56 -0500

    saving progress on Video 3

--- NeuralProbabilisticLangeModel/main.cpp
+++ NeuralProbabilisticLangeModel/main.cpp
@@ -47,6 +47,7 @@ int main()
 	for (auto lv : label_vector)
 	{
 		Y[rowCount] = mm.Stoi()[lv];
+		rowCount++;
 	}
 
 	rowCount = 0;
@@ -59,10 +60,47 @@ int main()
 			X[rowCount][colCount] = mm.Stoi()[c];
 			colCount++;
 		}
+
+		rowCount++;
 	}
 
-	std::cout << "X sizes: " << X.sizes() << std::endl;
-	std::cout << "Y sizes: " << Y.sizes() << std::endl;
+	torch::Tensor C = torch::randn({27, 2}, torch::kFloat32);
+	//std::cout << C << std::endl;
+	torch::Tensor F = torch::one_hot(torch::tensor(5), 27).to(torch::kFloat32);
+	std::cout << F.t().unsqueeze(0).mm(C) << std::endl;
+
+	torch::Tensor Embeddings = torch::zeros({ static_cast<long>(context_vector.size()), blockSize, C.size(1)}, torch::kFloat32);
+
+	rowCount = 0;
+	for (auto cv : context_vector)
+	{
+		int colCount = 0;
+
+		for (auto c : cv)
+		{
+			X[rowCount][colCount] = mm.Stoi()[c];
+			Embeddings[rowCount][colCount] = C[mm.Stoi()[c]];
+			colCount++;
+		}
+
+		rowCount++;
+	}
+	
+	std::cout << X[13][2] << std::endl;
+	std::cout << C[1] << std::endl;
+	std::cout << Embeddings[13][2] << std::endl;
+
+	torch::Tensor W1 = torch::randn({ 6, 100 });
+	torch::Tensor b = torch::randn({ 100 });
+
+
+
+	auto t = torch::cat({ Embeddings.index({torch::indexing::Slice(), 0, torch::indexing::Slice()}),
+						Embeddings.index({ torch::indexing::Slice(), 1, torch::indexing::Slice() }),
+						Embeddings.index({ torch::indexing::Slice(), 2, torch::indexing::Slice() }) }, 1);
+
+	std::cout << "t sizes" << t.sizes() << std::endl;
+	std::cout << t << std::endl;
 
 	return 0;
 }
