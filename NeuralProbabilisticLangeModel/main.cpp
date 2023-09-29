85c0ea0b5053ccca2f699f73d0158194d8d00ee4
shtuk41 <shtuk41@gmail.com>
2023-09-28 21:52:53 -0500

    finished video 3

--- NeuralProbabilisticLangeModel/main.cpp
+++ NeuralProbabilisticLangeModel/main.cpp
@@ -56,7 +56,7 @@ int main()
 		torch::Tensor b2 = torch::randn(27, torch::kFloat32);
 		b2.set_requires_grad(true);
 
-		for (int run = 0; run < 100; run++)
+		for (int run = 0; run < 50001; run++)
 		{
 			auto ix = torch::randint(0, context_vector.size(), { 32, });
 
@@ -71,8 +71,7 @@ int main()
 
 				for (auto c : cv)
 				{
-					torch::Tensor s = C[mm.Stoi()[c]];
-					Embeddings[rowCount][colCount] = s;
+					Embeddings[rowCount][colCount] = C[mm.Stoi()[c]];
 					colCount++;
 				}
 
@@ -90,19 +89,24 @@ int main()
 			//torch::Tensor loss = -prob.index({ torch::arange((int)context_vector.size(), torch::kInt64), Y }).log().mean();// +0.01 * torch::mm(W, W).mean();
 			torch::Tensor loss = torch::nn::functional::cross_entropy(logits, Y, torch::nn::functional::CrossEntropyFuncOptions().reduction(torch::kMean));
 
-			std::cout << "Loss is: " << loss.item() << std::endl;
+			if (run % 10000 == 0)
+			{
+				std::cout << "Run: " << run << " Loss is : " << loss.item() << std::endl;
+			}
 
 			loss.backward();
 
-			W1.set_data(W1.data() - 0.1 * W1.grad());
+			float learningRate = 0.1f;
+
+			W1.set_data(W1.data() - learningRate * W1.grad());
 			W1.grad().zero_();
-			W2.set_data(W2.data() - 0.1 * W2.grad());
+			W2.set_data(W2.data() - learningRate * W2.grad());
 			W2.grad().zero_();
-			b1.set_data(b1.data() - 0.1 * b1.grad());
+			b1.set_data(b1.data() - learningRate * b1.grad());
 			b1.grad().zero_();
-			b2.set_data(b2.data() - 0.1 * b2.grad());
+			b2.set_data(b2.data() - learningRate * b2.grad());
 			b2.grad().zero_();
-			C.set_data(C.data() - 0.1 * C.grad());
+			C.set_data(C.data() - learningRate * C.grad());
 			C.grad().zero_();
 		}
 
@@ -132,11 +136,50 @@ int main()
 
 		std::cout << "Full Loss is: " << loss << std::endl;
 
+
+		std::string context = start_context;
+
+		int countNames = 0;
+
+		while (countNames < 20)
+		{
+			std::string name;
+
+			while (true)
+			{
+				torch::Tensor emb = torch::zeros({ blockSize, C.size(1) }, torch::kFloat32);
+
+				for (int n = 0; n < blockSize; n++)
+				{
+					emb[n] = C[mm.Stoi()[context[n]]];
+				}
+
+				torch::Tensor lh = torch::tanh(torch::mm(emb.view({ 1, -1 }), W1) + b1);
+				torch::Tensor llogits = torch::mm(lh, W2) + b2;
+				torch::Tensor lprobs = torch::softmax(llogits, 1);
+				int lix = torch::multinomial(lprobs, 1, true).item<int>();
+				context += mm.Itos()[lix];
+				context = context.erase(0, 1);
+				name += mm.Itos()[lix];
+
+				if (lix == 0)
+					break;
+			}
+
+			if (name.size() > 1)
+			{
+				std::cout << name << std::endl;
+				countNames++;
+			}
+		}
+
 		Embeddings.reset();
 		Y.reset();
 		h.reset();
 		logits.reset();
 		loss.reset();
+
+		
 	}
 	catch (const c10::Error &e)
 	{
